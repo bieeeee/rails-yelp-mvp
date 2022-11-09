1c2c8ce5f126bb86205d1adfc8aeb3a8c3ccf4ff
hanbyeol kwon <janviertrois@naver.com>
2022-11-09 13:40:35 -0500

    controller, routes, views completed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: %i[new create]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(params_review)
+    @review.restaurant = @restaurant
+
+    if @review.save
+      redirect_to restaurant_path(@review.restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def params_review
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 end
